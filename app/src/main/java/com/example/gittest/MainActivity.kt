17ec2403dbd4a79efbbdd50539fe349f0b84e6c5
sw622 <sw622@naver.com>
2022-01-24 02:42:24 +0900

    Button 변수 추가

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button1 : android.widget.Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Button 클릭 이벤트 작성
     }
 }
